4575dd7c74b5102aca4bd0f0cef62699297eda77
Rushan <xamidullin.rushan@gmail.com>
2024-11-11 19:29:59 +0300

    0.0

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Squad Game</title>
+</head>
+<body>
+    <h1>Проверка</h1>
+    <?php include 'header.php'; ?>
+</body>
+</html>
